3e19cd0bf35f25f35bbc5d33166d34cba7ae80a8
Călin Ardelean <mmn80cpu@gmail.com>
2016-05-12 17:51:30 +0300

    command line options

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,37 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Control.Monad.State   (get, put)
+import Control.Monad         (when)
+import Data.List             (intersperse)
+import System.Console.Docopt
+import System.Environment    (getArgs)
 import Yi
+
+help :: Docopt
+help = [docopt|
+    Usage:
+      yi [<file> ...]
+      yi (-h|--help)
+
+    Options:
+      -h, --help   Show usage
+|]
 
 main :: IO ()
 main = do
---    args <- parseArgsOrExit help =<< getArgs
---    let files = getAllArgs args (argument "file")
---        actions = intersperse (EditorA newTabE) (map (YiA . openNewFile) files)
-    startEditor (myConfig {-actions-}) Nothing
+  args <- parseArgsOrExit help =<< getArgs
+  when (args `isPresent` (longOption "help") || args `isPresent` (shortOption 'h')) $
+    exitWithUsage help
+  let files = getAllArgs args (argument "file")
+      actions = intersperse (EditorA newTabE) (map (YiA . openNewFile) files)
+  startEditor (myConfig actions) Nothing
 
-myConfig :: {-[Action] ->-} Config
-myConfig {-actions-} = defaultEmacsConfig
-    -- { modeTable =
-    --     fmap
-    --         (configureModeline . configureIndent)
-    --         (myModes defaultVimConfig)
-    -- , defaultKm = myKeymapSet
-    -- , configCheckExternalChangesObsessively = False
-    -- , startActions =
-    --     (EditorA (do
-    --         e <- get
-    --         put e { maxStatusHeight = 30 }))
-    --     : YiA guessMakePrg
-    --     : YiA startIdrisIDE
-    --     : actions
-    -- }
+myConfig :: [Action] -> Config
+myConfig actions = defaultEmacsConfig
+  { configCheckExternalChangesObsessively = False
+  , startActions =
+      (EditorA (do
+        e <- get
+        put e { maxStatusHeight = 30 }))
+    : actions
+  }
